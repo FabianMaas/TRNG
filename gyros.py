50f52d796bba2b80c51238d0ba309224ccc1bb49
Swen Schlicksupp <s5kelgam@gmail.com>
2023-05-23 11:01:31 +0200

    fix10

--- gyros.py
+++ gyros.py
@@ -64,14 +64,14 @@ if __name__ == "__main__":
             acclY_scaled = acc_y * .000061 * 9.80665
             acclZ_scaled = acc_z * .000061 * 9.80665
              
-            x_angle += round(get_x_rotation(acclX_scaled, acclY_scaled, acclZ_scaled),0)
-            y_angle += round(get_y_rotation(acclX_scaled, acclY_scaled, acclZ_scaled),0)
+            x_angle += round(get_x_rotation(acclX_scaled, acclY_scaled, acclZ_scaled),0) + 83
+            y_angle += round(get_y_rotation(acclX_scaled, acclY_scaled, acclZ_scaled),0) - 96
             count += 1
             
             #print("X rotation: ", round(x_angle, 2))
             #print("Y rotation: ", round(y_angle, 2))
-            print("Y-Mittelwert: " +str(y_angle/count - 96))
-            print("X-Mittelwert: " +str(x_angle/count + 83))
+            print("Y-Mittelwert: " +str(y_angle/count))
+            print("X-Mittelwert: " +str(x_angle/count))
             sleep(.50)
     except KeyboardInterrupt:
         sys.exit(0)
